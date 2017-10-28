a6665c1fc56c3c7be7e3353316c2b5e26c07b91d
joelchucn <joelchu@foxmail.com>
2017-10-28 12:50:05 +0100

    adding the new top level files

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * This will be come the main export file
+ * When the consumer call it
+ * const gulpServerIo = require('gulp-server-io')
+ * When they want to use the underlying connect server
+ * const server = require('gulp-server-io/server');
+ * neat!
+ */
