de75ea475f2be9aa3b0b0885e24b4878abc58864
joelchucn <joelchu@foxmail.com>
2017-10-28 15:52:46 +0100

    adding the first example setup

---
+++ __tests__/examples/site.js
@@ -0,0 +1,20 @@
+'use strict';
+const path = require('path');
+const http = require('http');
+const chalk = require('chalk');
+const connect = require('connect');
+const serveStatic = require('serve-static');
+
+const root = path.join(__dirname, '..', 'fixtures', 'app');
+const app = connect();
+
+app.use(
+  serveStatic(root, {
+    'index': ['index.html', 'index.htm']
+  })
+);
+const server = http.createServer(app);
+
+server.listen(3001, () => {
+  console.log(chalk.yellow('Example server start @ 3001'));
+});
