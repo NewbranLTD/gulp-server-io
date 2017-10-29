c546b3d651fbe9d916f36ec01f1d3d10af34be66
joelchucn <joelchu@foxmail.com>
2017-10-29 14:03:07 +0000

    getting the mock json server setup

---
+++ src/lib/mock.js
@@ -0,0 +1,25 @@
+/**
+ * Mocking API call using json-server
+ */
+const chalk = require('chalk');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const logutil = require('./log.js');
+
+module.export = function(opt) {
+  const router = jsonServer.router(opt.json);
+  server.use(middlewares);
+  server.use(router);
+  // Apply more custom middlewares from config
+  if (opt.middlewares && Array.isArray(opt.middlewares) && opt.middlewares.length > 0) {
+    opt.middlewares.map(middleware => server.use(middleware));
+  }
+  // If for some reason why only want the server object
+  if (opt.serverOnly) {
+    return server;
+  }
+  server.listen(opt.port, () => {
+    logutil(chalk.white('[JSON Server is running]', opt.port));
+  });
+};
