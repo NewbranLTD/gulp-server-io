09f1bf9e97f2759e4b10b933bcb830af352ee851
joelchucn <joelchu@foxmail.com>
2017-11-02 22:44:18 +0000

    change path still produce error

--- __tests__/standalone/2-option.test.js
+++ __tests__/standalone/2-option.test.js
@@ -7,19 +7,19 @@ const request = require('supertest');
 const standaloneSrv = require('../../server');
 // Properties
 const root = path.join(__dirname, '..', 'fixtures', 'app');
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 // change port to 3838
 describe('Testing the gulp-server-io/server with different port number 3838', () => {
-  let server;
+  let server1;
   const url1 = 'http://localhost:3838';
   beforeEach(() => {
-    server = standaloneSrv({
+    server1 = standaloneSrv({
       webroot: root,
       port: 3838
     });
   });
   afterEach(() => {
-    server.close();
+    server1.close();
   });
   test(`It should connect to ${url1}`, () => {
     return request(url1)
@@ -28,23 +28,22 @@ describe('Testing the gulp-server-io/server with different port number 3838', ()
   });
 });
 // change path to /sub
-/*
 describe('Testing the gulp-server-io/server serve from different path /sub', () => {
-  let server;
-  const url2 = 'http://localhost:8000/sub';
+  let server2;
+  const url2 = 'http://localhost:8000';
+  const path2 = '/sub';
   beforeEach(() => {
-    server = standaloneSrv({
+    server2 = standaloneSrv({
       webroot: root,
-      path: '/sub'
+      path: path2
     });
   });
   afterEach(() => {
-    server.close();
+    server2.close();
   });
-  test(`It should connect to ${url2}`, () => {
+  test(`It should connect to ${url2}${path2}`, () => {
     return request(url2)
-      .get('/')
+      .get(path2)
       .expect(200, /Bootstrap Template test for gulp-server-io/);
   });
 });
-*/
