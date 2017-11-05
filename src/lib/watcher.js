9a904a749e776d4a1cc5fa27303b3f0810e3e540
joelchucn <joelchu@foxmail.com>
2017-11-05 11:46:15 +0000

    finish the reload part

--- src/lib/watcher.js
+++ src/lib/watcher.js
@@ -2,13 +2,11 @@
 /**
  * New file watcher
  */
-// const _ = require('lodash');
-// Const args = require('yargs');
 const chalk = require('chalk');
 const bacon = require('baconjs');
 const reload = require('reload');
 const chokidar = require('chokidar');
-const log = require('./utils/log');
+const logutil = require('./utils/log');
 /**
  * @param {string} webroot path to watch
  * @param {object} app express app
@@ -17,11 +15,12 @@ const log = require('./utils/log');
  */
 module.exports = function(webroot, app, config = {}) {
   const reloadServer = reload(app, config);
-  const verbose = config.reload.verbose;
+  const verbose = config.verbose;
+
   let watcher;
   let files = [];
   if (verbose) {
-    log(chalk.white('[Watcher]'), webroot);
+    logutil(chalk.white('[Watcher]'), webroot);
   }
   // Start the watch files with Bacon wrapper
   const streamWatcher = bacon.fromBinder(sink => {
@@ -38,17 +37,14 @@ module.exports = function(webroot, app, config = {}) {
   });
   // Reactive
   return streamWatcher
-    .doAction(a => {
-      log('action', a);
-      files.push(a);
-    })
+    .doAction(a => files.push(a))
     .debounce(300)
     .onValue(() => {
       if (files.length) {
         if (verbose) {
-          log(chalk.white('[Watcher]'), 'File changed');
+          logutil(chalk.white('[Watcher]'), 'File changed');
           files.forEach(f => {
-            log(chalk.yellow(f.event), chalk.yellow(f.path));
+            logutil(chalk.yellow(f.event), chalk.yellow(f.path));
           });
         }
         reloadServer.reload();
