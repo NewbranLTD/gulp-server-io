b64590791ae940a9b497d3f90e3822e73fe75e5b
joelchucn <joelchu@foxmail.com>
2017-11-20 16:38:57 +0000

    add cli test

---
+++ __tests__/funcs/cli.test.js
@@ -0,0 +1,15 @@
+'use strict';
+/**
+ * test the cli method
+ */
+const spawn = require('child_process').spawn;
+// we are going run directly against the node in cmd
+const cli = '../../cli.js';
+
+describe('Test the cli', () => {
+  
+
+
+
+
+});
