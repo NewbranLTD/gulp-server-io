5446c56e50147a1b552b3d2f394e1088d6c06af3
joelchu <joelckchu@gmail.com>
2018-03-04 15:52:12 +0800

    change the TLS var but still not working with https

--- __tests__/main/5-advance.test.js
+++ __tests__/main/5-advance.test.js
@@ -15,7 +15,7 @@ const {
   directoryIndexMissingDir
 } = require('../fixtures/config.js');
 // Some configuration to enable https testing
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 // Test start
 describe('gulp-webserver-io stock test', () => {
   // Setups
