613be5d4dfdec2ef52eb571d1f551a469e154e14
joelchu <joelckchu@gmail.com>
2018-03-19 16:25:29 +0800

    add export.js and going to remove gulp.js in the final version

--- src/lib/utils/stream-watcher.js
+++ src/lib/utils/stream-watcher.js
@@ -3,6 +3,7 @@
  * Seperate the watch method and report it
  * useful for other
  */
+const fs = require('fs');
 const chalk = require('chalk');
 const bacon = require('baconjs');
 const chokidar = require('chokidar');
@@ -10,6 +11,12 @@ const debug = require('debug')('gulp-webserver-io:stream-watcher');
 // Our tools
 const logutil = require('./log');
 const { toArray } = require('./helper');
+
+// Make sure to pass directories to this method
+const ensureIsDir = filePaths => {
+  return toArray(filePaths).filter(f => fs.lstatSync(f).isDirectory());
+};
+
 /**
  * Watch folder method
  * @param {array} filePaths to watch
@@ -17,24 +24,27 @@ const { toArray } = require('./helper');
  * @return {function} bacon method
  */
 module.exports = function(filePaths, verbose) {
-  return bacon.fromBinder(sink => {
-    toArray(filePaths).forEach(file => {
-      const webroot = file.path;
-      debug('[Watcher][webroot]', webroot);
-      if (verbose) {
-        logutil(chalk.white('[Watcher]'), webroot);
-      }
-      let watcher = chokidar.watch(webroot, {
-        ignored: /(^|[\/\\])\../,
-        ignoreInitial: true
-      });
-      watcher.on('all', (event, path) => {
-        debug('[Watcher][on]', event, path);
-        sink({ event: event, path: path });
-        return () => {
-          watcher.close();
-        };
+  const directories = ensureIsDir(filePaths);
+  if (directories.length) {
+    return bacon.fromBinder(sink => {
+      directories.forEach(dir => {
+        debug('[Watcher][webroot]', dir);
+        if (verbose) {
+          logutil(chalk.white('[Watcher]'), dir);
+        }
+        let watcher = chokidar.watch(dir, {
+          ignored: /(^|[\/\\])\../,
+          ignoreInitial: true
+        });
+        watcher.on('all', (event, path) => {
+          debug('[Watcher][on]', event, path);
+          sink({ event: event, path: path });
+          return () => {
+            watcher.close();
+          };
+        });
       });
     });
-  });
+  }
+  throw new Error('[streamWatcher] You must pass diretory to this function');
 };
