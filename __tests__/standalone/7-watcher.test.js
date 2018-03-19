ebde085c195063605450072bacf56b220df3a6e1
joelchu <joelckchu@gmail.com>
2018-03-19 15:23:18 +0800

    finish the basic test case

--- __tests__/standalone/7-watcher.test.js
+++ __tests__/standalone/7-watcher.test.js
@@ -5,18 +5,35 @@
 const { watcher } = require('../../gulp');
 const fs = require('fs-extra');
 const { join } = require('path');
+const target = 'package.json';
 const dir = join('..', 'fixtures', 'directoryIndexMissing');
-
+const file = join('..', 'fixtures', 'directoryIndexMissing', target);
+const data = {version: '1.0.1'};
+// Run
 describe('Testing the exported watcher [stream watcher] function', () => {
   let closeFn;
-  beforeAll( () => {
+  beforeAll( (done) => {
     closeFn = watcher(dir, function(files) {
-      console.log(files);
+      files.forEach( f => {
+        if (f.indexOf(target) > -1) {
+          fs.writeJsonSync(file, data);
+        }
+      });
     });
+    // Wait 1 second then touch the content in the folder
+    setTimeout( () => {
+      fs.writeJsonSync(file, {todo: true});
+      done();
+    }, 1000);
+  });
+  // clean up
+  afterAll( () => {
+    closeFn();
+    fs.writeJsonSync(file, {version: '1.0.0', todo: false});
   });
 
   it('Should able to see files changed and react', () => {
-    
+    expect(fs.readJsonSync(file)).toHaveProperty(data);
   });
 
 });
