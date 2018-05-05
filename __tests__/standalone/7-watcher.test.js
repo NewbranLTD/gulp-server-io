aac940802aa0b658578a341d7fd1742932c858a4
joelchucn <joelchu@foxmail.com>
2018-05-05 17:01:36 +0800

    The watcher throw some strange error

--- __tests__/standalone/7-watcher.test.js
+++ __tests__/standalone/7-watcher.test.js
@@ -2,41 +2,46 @@
 /**
  * Testing the exported stream watcher function
  */
-const { watcher } = require('../../export');
+const { fileWatcher } = require('../../export');
 const fs = require('fs-extra');
 const { join } = require('path');
 const { directoryIndexMissingDirRaw } = require('../fixtures/config.js');
 
 const target = 'package.json';
 const pkgFile = join(directoryIndexMissingDirRaw, target);
+const dest = join(__dirname, '..', '.tmp', 'package.json');
+fs.copySync(pkgFile, dest);
+// New 1.4.0-beta.11 copy this to a .tmp folder and don't touch the original
 const data = {version: '1.0.1', todo: false};
 // Run
 describe('Testing the exported watcher [stream watcher] function', () => {
   let closeFn;
   beforeAll( (done) => {
-    closeFn = watcher(directoryIndexMissingDirRaw, function(files) {
+    closeFn = fileWatcher(dest, function(files) {
       files.forEach( f => {
         // console.log('file change', f);
         if (f.path.indexOf(target) > -1) {
           // console.log('target touched');
-          fs.writeJsonSync(pkgFile, data);
+          fs.writeJsonSync(dest, data);
           done();
         }
       });
     });
     // Wait 1 second then touch the content in the folder
     setTimeout( () => {
-      fs.writeJsonSync(pkgFile, {todo: true});
+      fs.writeJsonSync(dest, {todo: true});
     }, 1000);
   });
   // clean up
   afterAll( () => {
     closeFn();
-    fs.writeJsonSync(pkgFile, {version: '1.0.0', todo: false});
+    // just remove the file
+    fs.remove(dest);
+    // fs.writeJsonSync(dest, {version: '1.0.0', todo: false});
   });
 
   test('Should able to see files changed and react', () => {
-    const content = fs.readJsonSync(pkgFile);
+    const content = fs.readJsonSync(dest);
     expect(content).toHaveProperty('version', '1.0.1');
   });
 
